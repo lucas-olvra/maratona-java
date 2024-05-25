c7f20a5599add1cd389dc078854746c5bcf0735a
lucas-olvra <lucas2000544@gmail.com>
2024-05-25 12:35:52 -0300

    Orientação a Objetos - Introdução a classes pt 02

---
+++ src/acadamy/devdojo/maratona/javacore/Aintroducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,15 @@
+package acadamy.devdojo.maratona.javacore.Aintroducaoclasses.test;
+
+import acadamy.devdojo.maratona.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 20;
+        estudante.sexo = 'M';
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
