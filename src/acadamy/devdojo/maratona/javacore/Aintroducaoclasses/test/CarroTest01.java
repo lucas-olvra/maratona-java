fadfc6dc1f6e478430b0bad44b4d60b2801ccaab
lucas-olvra <lucas2000544@gmail.com>
2024-05-25 14:24:33 -0300

    Exercício Orientação a Objeto

---
+++ src/acadamy/devdojo/maratona/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -0,0 +1,26 @@
+package acadamy.devdojo.maratona.javacore.Aintroducaoclasses.test;
+
+import acadamy.devdojo.maratona.javacore.Aintroducaoclasses.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+
+        carro1.nome = "HRV";
+        carro1.ano = 2016;
+        carro1.modelo = "Honda";
+
+        carro2.nome = "Elantra";
+        carro2.ano = 2015;
+        carro2.modelo = "Hyndai";
+
+        System.out.println(carro1.modelo);
+        System.out.println(carro1.ano);
+        System.out.println(carro1.nome);
+        System.out.println("\nCarro 2");
+        System.out.println(carro2.modelo);
+        System.out.println(carro2.ano);
+        System.out.println(carro2.nome);
+    }
+}
