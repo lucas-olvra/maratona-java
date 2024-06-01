c05ce547a2f2dcc939d979faaacecf879bd27685
Lucas Oliveira <lucas2000544@gmail.com>
2024-06-01 19:35:04 -0300

    Métodos PT 06 - Parâmetros por referência

---
+++ src/acadamy/devdojo/maratona/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -0,0 +1,25 @@
+package acadamy.devdojo.maratona.javacore.Bintroducaometodos.test;
+
+import acadamy.devdojo.maratona.javacore.Bintroducaometodos.dominio.Estudante;
+import acadamy.devdojo.maratona.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+        ImpressoraEstudante impressoraEstudante = new ImpressoraEstudante();
+
+        estudante01.nome = "Midorya";
+        estudante01.idade = 15;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Sakura";
+        estudante02.idade = 16;
+        estudante02.sexo = 'F';
+
+        impressoraEstudante.imprimi(estudante01);
+        impressoraEstudante.imprimi(estudante02);
+
+
+    }
+}
