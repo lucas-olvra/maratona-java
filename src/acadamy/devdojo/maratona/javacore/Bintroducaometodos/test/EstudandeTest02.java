df434f8f025af86a04ae55f68b82906a73e522b1
Lucas Oliveira <lucas2000544@gmail.com>
2024-06-02 12:23:48 -0300

    Métodos PT 08 - Referência This

---
+++ src/acadamy/devdojo/maratona/javacore/Bintroducaometodos/test/EstudandeTest02.java
@@ -0,0 +1,22 @@
+package acadamy.devdojo.maratona.javacore.Bintroducaometodos.test;
+
+import acadamy.devdojo.maratona.javacore.Bintroducaometodos.dominio.Estudante;
+import acadamy.devdojo.maratona.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudandeTest02 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+
+        estudante01.nome = "Midorya";
+        estudante01.idade = 15;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Sakura";
+        estudante02.idade = 16;
+        estudante02.sexo = 'F';
+
+        estudante01.imprime();
+        estudante02.imprime();
+    }
+}
