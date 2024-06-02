7adad4c401a2a609de42102fb351de2ced691cff
Lucas Oliveira <lucas2000544@gmail.com>
2024-06-02 12:45:15 -0300

    Métodos PT 08 - Varargs

---
+++ src/acadamy/devdojo/maratona/javacore/Bintroducaometodos/test/CalculadoraTest05.java
@@ -0,0 +1,13 @@
+package acadamy.devdojo.maratona.javacore.Bintroducaometodos.test;
+
+import acadamy.devdojo.maratona.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest05 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int[] numeros = {1,2,3,4,5};
+        calculadora.somaArray(numeros);
+
+        calculadora.somaVarArgs(1,2,3,4,5,6,7);
+    }
+}
